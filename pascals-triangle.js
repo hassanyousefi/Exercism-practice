d835d2113e53daaaa05727fc735bd8ab535e9752
HUsefi <hyousefi412@gmail.com>
2020-04-02 19:01:24 +0430

    edit pascal

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -4,22 +4,24 @@
 //
 
 export const rows = (numb) => {
-// var i , j , temp, main,sum;
-// temp=[1];
-// main=[];
-// sum=[];
-// for(i=1; i<=numb ; i++){
-//     for(j=0 ; j<=i ; j++){
-//         if(j==0||j==i){
-//             main[j]=1;
-//         }
-//         else{     
-//             main[j]=temp[j]+temp[j-1];  
-//         }
-//     }
-//     sum[i]=sum.concat("["+temp+"]");
-//     temp=Object.assign([],main);
+var i , j , temp, main,sum;
+temp=[1];
+main=[];
+sum=[];
+
+for(i=1; i<=numb ; i++){
+    for(j=0 ; j<=i ; j++){
+        if(j==0||j==i){
+            main[j]=1;
+        }
+        else{     
+            main[j]=temp[j]+temp[j-1];  
+        }
+    }
+    sum.push(temp);
+    temp=Object.assign([],main);
     
-// }
-return [];
+}
+
+return sum;
 };
